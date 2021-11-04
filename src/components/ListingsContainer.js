f633551bb0ae98ed2925d778a3e6d12be6658c66
Christopher <c.barnhurst@live.com>
2021-11-03 22:20:22 -0500

    commit tree 0532485d2167353719cb40e15683d1fc72697dd5

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,34 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((res) => res.json())
+      .then((listings) => setListings(listings));
+  }, []);
+
+  function handleDeleteListing(id) {
+      const updatedListingArray = listings.filter(
+        (listing) => listing.id !== id
+        );
+        setListings(updatedListingArray);
+  } 
+
+  const filteredListings = listings.filter(listing => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+  const listingCards = filteredListings.map((listingObj) => {
+    return <ListingCard 
+    key={listingObj.id} 
+    listing={listingObj}
+    onDeleteListing={handleDeleteListing}/>
+  })
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <ul className="cards">{listingCards}</ul>
     </main>
   );
 }
